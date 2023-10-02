892e466d5489e91c443264e48117199648561e10
Alina Vinogradova <jsemaljaa@gmail.com>
2023-10-02 19:38:14 +0200

    main module file

---
+++ src/compiler.c
@@ -0,0 +1,10 @@
+/*
+ * compiler.c
+ *
+ * @brief Main module
+ * @author Alina Vinogradova <xvinog00@vutbr.cz>
+ */
+
+int main(int argc, const char **argv){
+    // currently will be used for dynamic strings testing
+}
